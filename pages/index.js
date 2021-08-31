759c679af9cc6e36b53d01e610adba24bf358a1f
Ritesh <riteshpuvvada@gmail.com>
2021-08-31 10:05:02 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Auth from '../components/Auth'
 import Chat from '../components/Chat'
