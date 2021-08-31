501090513559047df284aa9731f21453c6cfa9fa
RiteshPuvvada <riteshpuvvada@gmail.com>
2021-08-31 10:04:08 +0530

    fixed

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Auth from '../components/Auth'
 import Chat from '../components/Chat'
+
 import { useEffect, useState } from 'react'
 
 export default function Home({ currentUser, session, supabase }) {
